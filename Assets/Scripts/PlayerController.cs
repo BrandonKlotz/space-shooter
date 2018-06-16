bb6ec7e52f8dfdc36836b055a38da6830dd383a6
Brandon Klotz <brandonklotz@icloud.com>
2018-06-16 09:41:07 -0400

    Manipulate Player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,28 +23,17 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-
+        
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-        {
-            Vector3 movement = Input.GetTouch(0).deltaPosition;
-            transform.Translate(-movement.x * speed, 0.0f, -movement.z * speed);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-            rb.velocity = movement * speed;
-            rb.position = new Vector3(Mathf.Clamp(rb.position.x, boundry.xMin, boundry.xMax), 0.0f,
-                                          Mathf.Clamp(rb.position.z, boundry.zMin, boundry.zMax));
-            rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+        rb.velocity = movement * speed;
+        rb.position = new Vector3(Mathf.Clamp(rb.position.x, boundry.xMin, boundry.xMax), 0.0f,
+                                      Mathf.Clamp(rb.position.z, boundry.zMin, boundry.zMax));
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
 
-        } else {
-            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-            rb.velocity = movement * speed;
-            rb.position = new Vector3(Mathf.Clamp(rb.position.x, boundry.xMin, boundry.xMax), 0.0f,
-                                          Mathf.Clamp(rb.position.z, boundry.zMin, boundry.zMax));
-            rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
-        }
     }
 
 }
