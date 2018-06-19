907052a8f441bc76c6aad62b57bc30d66444a151
Brandon Klotz <brandonklotz@icloud.com>
2018-06-19 17:22:34 -0400

    Add Explosions to Game Objects

--- Assets/Scripts/DestroyByBoundary.cs
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyByBoundary : MonoBehaviour{
+    
     void OnTriggerExit(Collider other){
         Destroy(other.gameObject);
     }
